26cb13242d8c68b551dd827aeabe6f679fffabca
Sahil Gagan <51903665+sahil2k19@users.noreply.github.com>
2025-03-19 11:28:55 +0530

    push

--- routes/blogs.controller.js
+++ routes/blogs.controller.js
@@ -1,111 +1,111 @@
-const Blogs = require("../database/models/BlogSection");
+// const Blogs = require("../database/models/BlogSection");
 
 
 
-const getAllBlogs = async (req, res) => {
-    try {
-        const blogs = await Blogs.find();
-        res.json(blogs);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
-}
-const addBlogs = async (req, res) => {
-    const body = req.body;
+// const getAllBlogs = async (req, res) => {
+//     try {
+//         const blogs = await Blogs.find();
+//         res.json(blogs);
+//     } catch (err) {
+//         console.error(err.message);
+//         res.status(500).send('Server Error');
+//     }
+// }
+// const addBlogs = async (req, res) => {
+//     const body = req.body;
 
-    if (!body) {
-        return res.status(400).json({
-            msg: 'No data'
-        });
-    }
+//     if (!body) {
+//         return res.status(400).json({
+//             msg: 'No data'
+//         });
+//     }
 
-    try {
-        const blog = new Blogs(body);
+//     try {
+//         const blog = new Blogs(body);
 
-        await blog.save();
+//         await blog.save();
 
-        return res.json(blog);
+//         return res.json(blog);
 
-    } catch (err) {
-        // console.error(err.message);
-       return  res.status(500).json({message:err.message});
-    }
-}
+//     } catch (err) {
+//         // console.error(err.message);
+//        return  res.status(500).json({message:err.message});
+//     }
+// }
 
-const getBlogData = async (req, res) => {
-    const {id } = req.params;
-    try {
-        const blog = await Blogs.findById(id);
+// const getBlogData = async (req, res) => {
+//     const {id } = req.params;
+//     try {
+//         const blog = await Blogs.findById(id);
 
-        if (!blog) {
-            return res.status(404).json({
-                msg: 'No blog found'
-            });
-        }
+//         if (!blog) {
+//             return res.status(404).json({
+//                 msg: 'No blog found'
+//             });
+//         }
 
-        return res.json(blog);
+//         return res.json(blog);
 
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
-}
+//     } catch (err) {
+//         console.error(err.message);
+//         res.status(500).send('Server Error');
+//     }
+// }
 
-const updateBlogData = async (req, res) => {
-    const { id } = req.params;
-    const body = req.body;
+// const updateBlogData = async (req, res) => {
+//     const { id } = req.params;
+//     const body = req.body;
 
-    if (!body) {
-        return res.status(400).json({
-            msg: 'No data'
-        });
-    }
+//     if (!body) {
+//         return res.status(400).json({
+//             msg: 'No data'
+//         });
+//     }
 
-    try {
-        const blog = await Blogs.findByIdAndUpdate(id, body, { new: true });
+//     try {
+//         const blog = await Blogs.findByIdAndUpdate(id, body, { new: true });
 
-        if (!blog) {
-            return res.status(404).json({
-                msg: 'No blog found'
-            });
-        }
+//         if (!blog) {
+//             return res.status(404).json({
+//                 msg: 'No blog found'
+//             });
+//         }
 
-        return res.json(blog);
+//         return res.json(blog);
 
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
-}
+//     } catch (err) {
+//         console.error(err.message);
+//         res.status(500).send('Server Error');
+//     }
+// }
 
-const deleteBlogData = async (req, res) => {
-    const { id } = req.params;
+// const deleteBlogData = async (req, res) => {
+//     const { id } = req.params;
 
-    try {
-        const blog = await Blogs.findByIdAndDelete(id);
+//     try {
+//         const blog = await Blogs.findByIdAndDelete(id);
 
-        if (!blog) {
-            return res.status(404).json({
-                msg: 'No blog found'
-            });
-        }
+//         if (!blog) {
+//             return res.status(404).json({
+//                 msg: 'No blog found'
+//             });
+//         }
 
-        return res.json({
-            msg: 'Blog deleted successfully'
-        });
+//         return res.json({
+//             msg: 'Blog deleted successfully'
+//         });
 
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
-}
+//     } catch (err) {
+//         console.error(err.message);
+//         res.status(500).send('Server Error');
+//     }
+// }
 
 
-module.exports = {
-    addBlogs,
-    getBlogData,
-    getAllBlogs,
-    updateBlogData,
-    deleteBlogData
-}
+// module.exports = {
+//     addBlogs,
+//     getBlogData,
+//     getAllBlogs,
+//     updateBlogData,
+//     deleteBlogData
+// }
